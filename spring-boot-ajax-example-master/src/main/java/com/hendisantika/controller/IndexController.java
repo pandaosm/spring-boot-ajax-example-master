6a099f797d5cebf991304d08693b8e39c634d5ac
OUSSAMA ELBAKKOURI <oussama.elbakkouri.external@atos.net>
2023-10-19 22:03:57 +0100

    add comment to index controller

--- spring-boot-ajax-example-master/src/main/java/com/hendisantika/controller/IndexController.java
+++ spring-boot-ajax-example-master/src/main/java/com/hendisantika/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+//index controller
     private final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @GetMapping("/")
